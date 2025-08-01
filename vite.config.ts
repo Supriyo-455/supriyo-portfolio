e2eef7a7945c5642f3f52f828c83502a3e8f60f6
Supriyo Paul <paulsupriyo64nk@gmail.com>
2025-08-01 16:22:22 +0530

    updated deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,25 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 //https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/supriyo-portfolio/',
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-}));
+// export default defineConfig(({ mode }) => ({
+//   base: '/supriyo-portfolio/',
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   build: {
+//     outDir: 'dist',
+//     assetsDir: 'assets',
+//   },
+// }));
 
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/supriyo-portfolio/', // Replace with your repository name
-// });
+export default defineConfig({
+  plugins: [react()],
+  base: '/supriyo-portfolio/', // Replace with your repository name
+});
